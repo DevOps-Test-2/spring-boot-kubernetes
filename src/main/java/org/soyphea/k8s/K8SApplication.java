07a6cafbc8ddf8ca262a6f79f1fb8ae261272128
NicolasJordanR <114710284+NicolasJordanR@users.noreply.github.com>
2022-10-27 22:18:59 -0300

    Update K8SApplication.java

--- src/main/java/org/soyphea/k8s/K8SApplication.java
+++ src/main/java/org/soyphea/k8s/K8SApplication.java
@@ -16,7 +16,18 @@ public class K8SApplication implements CommandLineRunner {
     UserConfig userConfig;
 
     public static void main(String[] args) {
+        int fran = 1;
+        if(fran==1 && fran == 8 && fran == 9){
+            fran = 2;
+        }else if(fran==1){
+            fran = 7;
+        }else if(fran==0){
+            fran=99;
+        }else{
+            fran=888;
+        }
         SpringApplication.run(K8SApplication.class, args);
+    
     }
 
     @Override
