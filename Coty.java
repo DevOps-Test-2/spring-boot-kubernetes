b09a5ac4dbb34a74bc0cdb26099034ad159f5200
NicolasJordanR <114710284+NicolasJordanR@users.noreply.github.com>
2022-10-27 21:56:50 -0300

    Create Coty.java

---
+++ Coty.java
@@ -0,0 +1,18 @@
+package org.soyphea.k8s;
+
+import lombok.extern.slf4j.Slf4j;
+import org.soyphea.k8s.config.UserConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class coty  {
+    private int n;
+
+    
+    public void controller(){
+    
+    }
+}
